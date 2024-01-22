f8d1961e120f11047160b1c910b80a6ac4fa0343
matheusman <pereiralimamatheuspl@gmail.com>
2024-01-21 23:25:27 -0300

    Get path

--- Program.cs
+++ Program.cs
@@ -8,31 +8,17 @@ namespace Course
 	{
 		public static void Main(string[] args)
 		{
-			try {
-
-				string line = WriteInConsole("Coloque seu tempo de horario em que esta no brasil: ");
-				DateTime dateTime = DateTime.Parse(line);
-
-				if (!(dateTime is DateTime)) {
-					throw new FormatException("Coloque uma exeção correta");
-				}
-
-				string text = "text1.txt";
-				string path = Path.GetFullPath("./" + text);
-				File.WriteAllLines(path, [dateTime.ToString(), DateTime.Now.ToString()]);
-				using (FileStream fs = new FileStream(path, FileMode.CreateNew)) {
-					File.Copy(path.Replace(text, "text2.txt"), path);
-				}
-
-			} catch (FormatException e) {
-				Console.WriteLine("Deu um erro " + e.Message);
-			} catch (IOException e) {
-				Console.WriteLine("File is not exists" + e.Message);
+			string path = @"D:\Downloads\Course_c#";
+			string folderName = @"\newFolder";
+			Directory.CreateDirectory(path + folderName);
+			string name = WriteInConsole("Coloque um nome do arquivo");
+			using (FileStream fs = File.Create(path + "\\" + name)) {
+				File.AppendAllText(path, "Testando o metodo");
 			}
-
 		}
 
-		public static string WriteInConsole (string value) {
+		public static string WriteInConsole(string value)
+		{
 			Console.Write(value);
 			string read = Console.ReadLine();
 			return read;
