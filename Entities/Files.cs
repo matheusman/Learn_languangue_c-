b49ed19ba989d38c9799278cdac1e6c2a615a98d
matheusman <pereiralimamatheuspl@gmail.com>
2024-01-21 23:09:34 -0300

    File path

---
+++ Entities/Files.cs
@@ -0,0 +1,7 @@
+namespace Course.Entities {
+	class FileSystemUsing {
+
+		
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Course.Entity;
+﻿using System.IO;
+using System.Runtime.Intrinsics.Arm;
+using System.Text.RegularExpressions;
 
 namespace Course
 {
@@ -6,17 +8,34 @@ namespace Course
 	{
 		public static void Main(string[] args)
 		{
-			Account acc1 = new Account(0, "Alex", 100.0);
-			BuinnesAccount acc2 = new BuinnesAccount(0, "Joao", 100.0, 200.00);
+			try {
 
-			//Upcasting
-			Account acc3 = new BuinnesAccount(0, "jorje", 100.0, 100.0);
+				string line = WriteInConsole("Coloque seu tempo de horario em que esta no brasil: ");
+				DateTime dateTime = DateTime.Parse(line);
 
-			//Downcasting
-			if (acc3 is BuinnesAccount) {
-				BuinnesAccount acc4 = (BuinnesAccount)acc3;
-				Console.WriteLine("Write");
+				if (!(dateTime is DateTime)) {
+					throw new FormatException("Coloque uma exeção correta");
+				}
+
+				string text = "text1.txt";
+				string path = Path.GetFullPath("./" + text);
+				File.WriteAllLines(path, [dateTime.ToString(), DateTime.Now.ToString()]);
+				using (FileStream fs = new FileStream(path, FileMode.CreateNew)) {
+					File.Copy(path.Replace(text, "text2.txt"), path);
+				}
+
+			} catch (FormatException e) {
+				Console.WriteLine("Deu um erro " + e.Message);
+			} catch (IOException e) {
+				Console.WriteLine("File is not exists" + e.Message);
 			}
+
+		}
+
+		public static string WriteInConsole (string value) {
+			Console.Write(value);
+			string read = Console.ReadLine();
+			return read;
 		}
 	}
 }
